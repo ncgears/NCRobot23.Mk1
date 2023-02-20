cc398a2c36c0545df4b70565f87d42b6274a6589
ncgears <1918ncgears@gmail.com>
2023-02-20 11:03:44 -0500

    Make sure burner is home before spatula to griddle

--- src/main/java/frc/team1918/robot/commands/fivesecondrule/fsr_moveSpatulaTo.java
+++ src/main/java/frc/team1918/robot/commands/fivesecondrule/fsr_moveSpatulaTo.java
@@ -2,11 +2,14 @@ package frc.team1918.robot.commands.fivesecondrule;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team1918.robot.subsystems.FiveSecondRuleSubsystem;
+import frc.team1918.robot.subsystems.StoveSubsystem;
 import frc.team1918.robot.subsystems.FiveSecondRuleSubsystem.spatulas;
+import frc.team1918.robot.modules.Burner.BurnerPositions;
 import frc.team1918.robot.modules.Spatula.SpatulaPositions;
 public class fsr_moveSpatulaTo extends CommandBase {
   // @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"}) //Dont add "unused" under normal operation
   private final FiveSecondRuleSubsystem m_fsr;
+  private final StoveSubsystem m_stove;
   private final spatulas m_spatula;
   private final SpatulaPositions m_position;
 
@@ -15,12 +18,13 @@ public class fsr_moveSpatulaTo extends CommandBase {
    * While disabled, this simply sets the position to home so that the robot doesn't attempt to go to other positions when enabled
    * @param subsystem The FiveSecondRule subsystem required by this command
    */
-  public fsr_moveSpatulaTo(FiveSecondRuleSubsystem subsystem, spatulas spatula, SpatulaPositions position) {
+  public fsr_moveSpatulaTo(FiveSecondRuleSubsystem subsystem, StoveSubsystem stove, spatulas spatula, SpatulaPositions position) {
     m_fsr = subsystem;
+    m_stove = stove;
     m_spatula = spatula;
     m_position = position;
     // Use addRequirements() here to declare subsystem dependencies.
-    // addRequirements(fsr);
+    addRequirements(stove);
   }
 
   // Allow the command to run while disabled
@@ -32,6 +36,9 @@ public class fsr_moveSpatulaTo extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    if (m_position==SpatulaPositions.GRIDDLE) { //Griddle position is requested, so make sure Burner is home!
+      m_stove.moveBurnerTo(BurnerPositions.HOME);
+    }
     m_fsr.moveSpatulaTo(m_spatula, m_position);
   }
 
