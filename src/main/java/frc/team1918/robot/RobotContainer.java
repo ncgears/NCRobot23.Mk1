30f8dc4077e5153b4118c6f74b284a5a2c443045
ncgears <1918ncgears@gmail.com>
2023-03-14 15:19:31 -0400

    Clean unused imports

--- src/main/java/frc/team1918/robot/RobotContainer.java
+++ src/main/java/frc/team1918/robot/RobotContainer.java
@@ -9,23 +9,17 @@ package frc.team1918.robot;
 
 //Global imports
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-// import edu.wpi.first.cscore.AxisCamera;
 import edu.wpi.first.cscore.HttpCamera;
-import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 import java.util.Map;
 
-import edu.wpi.first.cameraserver.CameraServer;
-// import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
+// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 //import edu.wpi.first.wpilibj.PowerDistribution;
